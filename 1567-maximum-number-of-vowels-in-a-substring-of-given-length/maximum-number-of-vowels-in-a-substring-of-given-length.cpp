06585b8412a5df03137a24ef7556e16f41490fbd
nazmulislam <nazmulislam93400@gmail.com>
2025-11-25 22:56:16 +0600

    Time: 14 ms (20.67%) | Memory: 13.3 MB (34.57%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,22 +1,19 @@
 /*
  Author:Nazmul Islam
  Using sliding window pattern
- Complexity: O(n) time & O(1) space 
+ Complexity: O(n) time & O(1) space
 */
 class Solution {
 public:
     int maxVowels(string s, int k) {
         unordered_set<char> vowelSet = {'a', 'e', 'i', 'o', 'u'};
-        int n = s.length();
-        int cnt = 0;
-        for (int i = 0; i < k; ++i) {
-            if (vowelSet.contains(s[i])) ++cnt;
-        }
-        int maxVowelCnt = cnt;
-        for (int i = k; i < n; ++i) {
-            if (vowelSet.contains(s[i])) ++cnt;
-            if (vowelSet.contains(s[i - k])) --cnt;
-            maxVowelCnt = max(maxVowelCnt, cnt);
+        int maxVowelCnt = INT_MIN;
+        for (int i = 0, currCnt = 0; i < s.length(); ++i) {
+            currCnt += (vowelSet.contains(s[i]));
+            if (i >= k) {
+                currCnt -= (vowelSet.contains(s[i - k]));
+            }
+            maxVowelCnt = max(maxVowelCnt, currCnt);
         }
         return maxVowelCnt;
     }
