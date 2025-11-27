e4f9e9c24f5db20543154098fe0e1fe8254e8596
nazmulislam <nazmulislam93400@gmail.com>
2025-11-27 15:02:39 +0600

    Time: 85 ms (47.96%) | Memory: 56.2 MB (58.17%) - LeetSync

---
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -0,0 +1,29 @@
+/*
+  Author:Nazmul Islam
+  Using sliding window
+  Complexity: O(n) time & O(n) space
+*/
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return helperFunction(nums, k) - helperFunction(nums, k - 1);
+    }
+
+private:    
+// to calculate the number of subarrays with at most k different integers
+    int helperFunction(vector<int>& a, int v) {
+        unordered_map<int, int> m;
+        int l = 0;
+        int cnt = 0;
+        for (int r = 0; r < a.size(); ++r) {
+            ++m[a[r]];
+            while (m.size() > v) {
+                if (--m[a[l]] == 0)
+                    m.erase(a[l]);
+                ++l;
+            }
+            cnt += (r - l + 1);
+        }
+        return cnt;
+    }
+};
