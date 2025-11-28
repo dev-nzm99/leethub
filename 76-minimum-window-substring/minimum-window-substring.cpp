f9aa435c214ae15f1706337279b1ae6408e2f6fa
nazmulislam <nazmulislam93400@gmail.com>
2025-11-28 22:36:17 +0600

    Time: 16 ms (30.12%) | Memory: 11.6 MB (41.52%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,9 +1,16 @@
+/*
+ Author:Nazmul Islam
+ submitted at Nov 28, 2025 22:24
+ Using sliding window and hash map
+ complexity: O(n+m) time & O(n+m) space
+*/
 class Solution {
 public:
     string minWindow(string s, string t) {
         int n = s.length(), k = t.length();
 
-        if(n<k)return "";
+        if (n < k)
+            return "";
 
         unordered_map<char, int> m;
         for (const char& c : t)
@@ -18,21 +25,20 @@ public:
             --m[s[r]];
             if (total == 0) {
                 while (true) {
-                    if (total > 0)
+                    if (m.contains(s[l]) && m[s[l]] == 0)
                         break;
-                    int subStrLen = r - l;
-                    if (subStrLen < resultStrInx[1] - resultStrInx[0]) {
-                        resultStrInx[0] = l;
-                        resultStrInx[1] = r;
-                    }
-                    if (m.contains(s[l]) && m[s[l]] >= 0) {
-                        ++total;
-                    }
                     ++m[s[l]];
                     ++l;
                 }
+                if (r - l < resultStrInx[1] - resultStrInx[0]) {
+                    resultStrInx[0] = l;
+                    resultStrInx[1] = r;
+                }
+                ++m[s[l]];
+                ++total;
+                ++l;
             }
         }
-        return (resultStrInx[1] >= n)? "": s.substr(resultStrInx[0], resultStrInx[1] - resultStrInx[0] + 1);
+        return (resultStrInx[1] >= n) ? "" : s.substr(resultStrInx[0], resultStrInx[1] - resultStrInx[0] + 1);
     }
 };
