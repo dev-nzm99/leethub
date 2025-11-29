87ad63157dd983c2568013b1e445bc90aace9f53
nazmulislam <nazmulislam93400@gmail.com>
2025-11-29 18:53:57 +0600

    Update search-insert-position.cpp

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& a, int key) {
-        int l=0,h=a.size()-1,indx=a.size();
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            if(a[mid]==key)return mid;
-            else if(a[mid]>key){
-                indx=mid;
-                h=mid-1;
-            }
-            else{
-               //if(l==h)indx=mid+1;
-               l=mid+1;
-               
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
             }
         }
-        return indx;
+        return l;
     }
 };
