0d9fc74ce5ec7a053f77f5a2602d532f91d60e46
nazmulislam <nazmulislam93400@gmail.com>
2025-11-30 20:02:40 +0600

    Time: 0 ms (100.00%) | Memory: 8.6 MB (50.63%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long l=0,h=x,ans;
-        if(x<2)return x;
-        while(l<=h){
-             long long mid=l+(h-l)/2;
-            if(mid*mid == x)return mid;
-            else if(mid*mid <x){
-                ans=mid;
-                l=mid+1;
-            }
-            else h=mid-1;
+        long long l = 1, r = x;
+        int res = 0;
+        while (l <= r) {
+            long long m = l + (r - l) / 2;
+            if (m * m == x)
+                return m;
+            else if (m * m > x) {
+                res = m - 1;
+                r = m - 1;
+            } else
+                l = m + 1;
         }
-        return ans;
+        return res;
     }
 };
