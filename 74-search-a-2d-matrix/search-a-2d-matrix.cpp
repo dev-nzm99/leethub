8d8eec4625d717ce3d0d069fc40a5eeac62c70b9
nazmulislam <nazmulislam93400@gmail.com>
2025-11-30 21:18:40 +0600

    Time: 0 ms (100.00%) | Memory: 13.4 MB (6.57%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,23 @@
+/*
+  Author:Nazmul Islam
+  complexity: O(log(row * col)) time & O(1) space
+*/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int l = 0, r = row * col - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            int val = matrix[m / col][m % col];
+            if (val == target)
+                return true;
+            else if (val > target)
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+        return false;
+    }
+};
