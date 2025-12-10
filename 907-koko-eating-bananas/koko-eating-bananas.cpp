2cf23f905054c7e99dab68c4b3c16744a8c0a2a3
nazmulislam <nazmulislam93400@gmail.com>
2025-12-10 23:41:45 +0600

    Time: 11 ms (41.31%) | Memory: 23.1 MB (12.57%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,22 +1,28 @@
+/*
+ Author:Nazuml Islam
+ Using binary search pattern(on answer)
+ Complexity O(Nlogm) time & O(1) space
+*/
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int k_min = 1;
-        int k_max = *max_element(piles.begin(), piles.end());
-        while (k_min <= k_max) {
-            int mid = k_min + (k_max - k_min) / 2;
+        int minSpeed = 1;
+        int maxSpeed = *max_element(piles.begin(), piles.end());
+        while (minSpeed < maxSpeed) {
+            int mid = minSpeed + (maxSpeed - minSpeed) / 2;
             if (canEat(piles, mid, h)) {
-                k_max = mid - 1;
+                maxSpeed = mid;
             } else
-                k_min = mid + 1;
+                minSpeed = mid + 1;
         }
-        return k_min;
+        return minSpeed;
     }
+
 private:
     bool canEat(vector<int>& piles, int k, int h) {
-        long long totalHour = 0.0;
+        int totalHour = 0;
         for (const auto& pile : piles) {
-            totalHour += ceil((double)pile / k);
+            totalHour += (int)ceil((double)pile / k);
         }
         return totalHour <= h;
     }
