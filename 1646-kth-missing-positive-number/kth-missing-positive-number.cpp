6ecb7310b816be3c56aace6524c94cc046d42e3a
nazmulislam <nazmulislam93400@gmail.com>
2025-12-11 23:27:43 +0600

    Update kth-missing-positive-number.cpp

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -7,7 +7,7 @@ public:
     int findKthPositive(vector<int>& arr, int k) {
         int l = 0;
         int r = arr.size() - 1;
-        int res = arr.size();  //for edge cases
+        int res = arr.size();  
         while (l <= r) {
             int m = l + (r - l) / 2;
             if ((arr[m] - m - 1) >= k) {
