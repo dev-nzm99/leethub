9e48873458d055ce71c777d33554c9aeb99e709d
nazmulislam <nazmulislam93400@gmail.com>
2026-01-03 00:27:17 +0600

    Create BFS_method.cpp

---
+++ 226-invert-binary-tree/BFS_method.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+/*
+ Author:Nazmul Islam
+ Using BFS(lavel order traversal) pattern
+ Complexity: O(n) time & space
+*/
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            if (node == NULL) // to prevent null pointer error
+                continue;
+
+            // Swap left and right childs
+            TreeNode* temp = node->left;
+            node->left = node->right;
+            node->right = temp;
+
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+        }
+        return root;
+    }
+};
